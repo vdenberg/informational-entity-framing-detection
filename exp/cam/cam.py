4bc688686a7e0c7ec49a4c6131679186b8bc62d9
vdberg <vdberg@cl.uni-heidelberg.de>
2020-03-19 11:02:07 +0100

    cam with bert

--- exp/cam/cam.py
+++ exp/cam/cam.py
@@ -232,9 +232,10 @@ logger.info(f"Starting LR: {LR}")
 eval_df = pd.DataFrame(index=list(range(len(folds))) + ['mean'], columns=['Acc', 'Prec', 'Rec', 'F1'])
 for fold_i, fold in enumerate(folds):
 
-    cl = ContextAwareClassifier(input_lang, fold['dev'], split_type=SPL, logger=logger, weights_matrix=WEIGHTS_MATRIX,
-                                batch_size=BATCH_SIZE, cp_dir=CHECKPOINT_DIR, learning_rate=LR,
-                                start_checkpoint=START_CHECKPOINT, step_size=1, gamma=0.95)
+    cl = ContextAwareClassifier(input_lang, fold['dev'], split_type=SPL, logger=logger, cp_dir=CHECKPOINT_DIR,
+                                weights_matrix=WEIGHTS_MATRIX, emb_dim=EMB_DIM, hidden_size=HIDDEN,
+                                batch_size=BATCH_SIZE, learning_rate=LR, start_checkpoint=START_CHECKPOINT,
+                                step_size=1, gamma=0.95)
 
     model = cl.model
 
--- lib/classifiers/ContextAwareClassifier.py
+++ lib/classifiers/ContextAwareClassifier.py
@@ -109,9 +109,9 @@ class ContextAwareModel(nn.Module):
 
 
 class ContextAwareClassifier():
-    def __init__(self, input_lang, dev, split_type='fan',
+    def __init__(self, input_lang, dev, logger=None, split_type='fan',
                  emb_dim=768, hidden_size=32, weights_matrix=None,
-                 batch_size=None, logger=None, cp_dir='models/checkpoints/cam',
+                 batch_size=None, cp_dir='models/checkpoints/cam',
                  learning_rate=0.001, start_checkpoint=0, step_size=1, gamma=0.75):
         self.start_epoch = start_checkpoint
         self.cp_dir = cp_dir
