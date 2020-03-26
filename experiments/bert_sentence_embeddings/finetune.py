3a40285d00c55cc0de47f4d2133705ff56832b45
vdberg <vdberg@cl.uni-heidelberg.de>
2020-03-26 13:03:05 +0100

    direct comparison to baseline

--- experiments/bert_sentence_embeddings/finetune.py
+++ experiments/bert_sentence_embeddings/finetune.py
@@ -80,7 +80,7 @@ parser = argparse.ArgumentParser()
 # TRAINING PARAMS
 parser.add_argument('-ep', '--n_epochs', type=int, default=5)
 parser.add_argument('-lr', '--learning_rate', type=float, default=2e-5)
-parser.add_argument('-s', '--seed', type=int, default='random')
+parser.add_argument('-s', '--seed', type=int, default=0)
 parser.add_argument('-load', '--load_from_ep', type=int, default=0)
 args = parser.parse_args()
 
@@ -97,7 +97,7 @@ NUM_LABELS = 2
 PRINT_EVERY = 50
 
 if SEED:
-    if SEED == 'random':
+    if SEED == 0:
         SEED_VAL = random.randint(0, 300)
     else:
         SEED_VAL = SEED
