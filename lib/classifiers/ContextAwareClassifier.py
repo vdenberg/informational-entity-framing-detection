d4772cf7fd0a217328d9ed01ee7476c251f38efc
vdberg <vdberg@cl.uni-heidelberg.de>
2020-03-31 11:47:36 +0200

    direct comparison to baseline

--- lib/classifiers/ContextAwareClassifier.py
+++ lib/classifiers/ContextAwareClassifier.py
@@ -59,7 +59,7 @@ class ContextAwareModel(nn.Module):
             for item in range(batch_size):
                 my_idx = target_idx[item]
                 print(input_tensor.shape)
-                print(target_idx.shape)
+                print(input_tensor[item, my_idx])
                 target_embeddings[item] = self.embedding(input_tensor[item, my_idx]).view(1, 1, -1)
         else:
             context_encoder_outputs = torch.zeros(self.input_size, batch_size, self.hidden_size * 2, device=self.device)
