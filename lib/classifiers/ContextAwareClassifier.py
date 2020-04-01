62d573e969575a3f8d472fe5986e884529f424fb
vdberg <vdberg@cl.uni-heidelberg.de>
2020-04-01 18:31:03 +0200

    direct comparison to baseline

--- lib/classifiers/ContextAwareClassifier.py
+++ lib/classifiers/ContextAwareClassifier.py
@@ -63,7 +63,7 @@ class ContextAwareModel(nn.Module):
 
             target_output_a = torch.zeros(batch_size, self.emb_size, device=self.device)
             for batch_id, position in enumerate(positions):
-                target_output_a[batch_id] = document_embeddings[position, batch_id, :]
+                target_output_a[batch_id] = context_as_embeddings[position, batch_id, :]
 
             target_output_b = torch.zeros(batch_size, self.emb_size, device=self.device)
             for batch_id, position in enumerate(positions):
