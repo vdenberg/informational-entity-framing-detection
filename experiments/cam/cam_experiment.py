b2fb6aea67b72818bb673e8feb53b9ddcc010ab5
vdberg <vdberg@cl.uni-heidelberg.de>
2020-04-01 16:33:28 +0200

    direct comparison to baseline

--- experiments/cam/cam_experiment.py
+++ experiments/cam/cam_experiment.py
@@ -502,7 +502,7 @@ for ep in range(1, int(N_EPOCHS+1)):
     cnm.save_model(epoch_name)
     dev_preds, dev_loss = cnm.predict(dev_batches)
     print(len(dev_preds), len(fold['dev'].label))
-    dev_mets, dev_perf = my_eval(fold['dev'].label, dev_preds, av_loss=av_loss, set_type='dev', name=epoch_name)
+    dev_mets, dev_perf = my_eval(fold['dev'].label.values, dev_preds, av_loss=av_loss, set_type='dev', name=epoch_name)
     logger.info(f'{dev_perf}')
 
     # check if best
