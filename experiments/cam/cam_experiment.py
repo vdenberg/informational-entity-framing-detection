ae14e8e06663210fefa5d771a7896af74b0c7a19
vdberg <vdberg@cl.uni-heidelberg.de>
2020-04-01 16:43:29 +0200

    direct comparison to baseline

--- experiments/cam/cam_experiment.py
+++ experiments/cam/cam_experiment.py
@@ -384,13 +384,13 @@ fold_2 = folds[1]
 logger.info(f"Convert data to indices for weights matrix")
 sent_id_map = {sent_id.lower(): sent_num_id + 1 for sent_num_id, sent_id in enumerate(data_w_embeds.index.values)}
 
-train_ids = fold_2['train'].index
-dev_ids = fold_2['dev'].index
-test_ids = fold_2['test'].index
+train_ids = fold_2['train'].id_num
+dev_ids = fold_2['dev'].id_num
+test_ids = fold_2['test'].id_num
 
-train_ids = [sent_id_map[i] for i in train_ids]
-dev_ids = [sent_id_map[i] for i in dev_ids]
-test_ids = [sent_id_map[i] for i in test_ids]
+#train_ids = [sent_id_map[i] for i in train_ids]
+#dev_ids = [sent_id_map[i] for i in dev_ids]
+#test_ids = [sent_id_map[i] for i in test_ids]
 
 '''
 # =====================================================================================
