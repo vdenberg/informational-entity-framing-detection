4bb42e29e4972a79c773012da563144c916458d3
vdberg <vdberg@cl.uni-heidelberg.de>
2020-04-01 15:30:22 +0200

    direct comparison to baseline

--- experiments/cam/cam_experiment.py
+++ experiments/cam/cam_experiment.py
@@ -394,7 +394,6 @@ test_data = TensorDataset(test_ids, test_labels)
 train_batches = to_batches(train_data, BATCH_SIZE)
 dev_batches = to_batches(dev_data, 1)
 test_batches = to_batches(test_data, 1)
-exit(0)
 # =====================================================================================
 #                    TRAIN CLASSIFIER
 # =====================================================================================
