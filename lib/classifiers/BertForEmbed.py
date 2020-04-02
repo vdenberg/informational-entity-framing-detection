703ba55bfcc7ba6d89374c60f6e2196b79d12522
vdberg <vdberg@cl.uni-heidelberg.de>
2020-04-02 13:54:49 +0200

    direct comparison to baseline

--- lib/classifiers/BertForEmbed.py
+++ lib/classifiers/BertForEmbed.py
@@ -17,7 +17,7 @@ def to_tensor(features, device):
 
     label_ids = torch.tensor([f.label_id for f in features], dtype=torch.long)
 
-    data = TensorDataset(input_ids, input_mask, segment_ids, label_ids)
+    data = TensorDataset(input_ids, input_mask, label_ids)
     return example_ids, data, label_ids  # example_ids, input_ids, input_mask, segment_ids, label_ids
 
 
--- lib/classifiers/BertWrapper.py
+++ lib/classifiers/BertWrapper.py
@@ -145,7 +145,7 @@ class BertWrapper:
 
     def train_on_batch(self, batch):
         batch = tuple(t.to(self.device) for t in batch)
-        _, _, input_ids, input_mask, label_ids = batch
+        input_ids, input_mask, label_ids = batch
 
         self.model.zero_grad()
         outputs = self.model(input_ids, input_mask, labels=label_ids)
@@ -164,7 +164,7 @@ class BertWrapper:
         sum_loss = 0
         for step, batch in enumerate(batches):
             batch = tuple(t.to(self.device) for t in batch)
-            _, _, input_ids, input_mask, label_ids = batch
+            input_ids, input_mask, label_ids = batch
 
             with torch.no_grad():
                 outputs = self.model(input_ids, input_mask, labels=None)
