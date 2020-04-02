05113bbd2c3b08953aad1a7e94db136eecc151b4
vdberg <vdberg@cl.uni-heidelberg.de>
2020-04-02 14:28:26 +0200

    direct comparison to baseline

--- experiments/bert_sentence_embeddings/finetune.py
+++ experiments/bert_sentence_embeddings/finetune.py
@@ -231,7 +231,7 @@ if __name__ == '__main__':
                     nb_tr_examples, nb_tr_steps = 0, 0
                     for step, batch in enumerate(train_dataloader):
                         batch = tuple(t.to(device) for t in batch)
-                        input_ids, input_mask, segment_ids, label_ids = batch
+                        input_ids, input_mask, label_ids = batch
                         # print(label_ids)
 
                         model.zero_grad()
