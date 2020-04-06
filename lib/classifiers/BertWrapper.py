0307cad6c4d7392cc1a75de7e36c66dc167ecaa2
vdberg <vdberg@cl.uni-heidelberg.de>
2020-04-06 10:46:14 +0200

    direct comparison to baseline

--- lib/classifiers/BertWrapper.py
+++ lib/classifiers/BertWrapper.py
@@ -34,7 +34,7 @@ class BertForSequenceClassification(BertPreTrainedModel):
 
         self.init_weights()
 
-        self.lstm = LSTM(self.input_size, self.hidden_size, num_layers=self.bilstm_layers, bidirectional=True)
+        #self.lstm = LSTM(self.input_size, self.hidden_size, num_layers=self.bilstm_layers, bidirectional=True)
 
     def forward(
         self,
