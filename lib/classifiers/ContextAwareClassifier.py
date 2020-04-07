e29b80fdc245af7e06a1ff8a7282f0df6f2f37a8
vdberg <vdberg@cl.uni-heidelberg.de>
2020-04-07 13:50:01 +0200

    direct comparison to baseline

--- lib/classifiers/ContextAwareClassifier.py
+++ lib/classifiers/ContextAwareClassifier.py
@@ -70,6 +70,7 @@ class ContextAwareModel(nn.Module):
 
         if self.context_naive:
             token_ids, token_mask = inputs
+            print(token_ids.shape)
             for seq_idx in range(seq_len):
                 t_i, t_m = token_ids[:, seq_idx], token_mask[:, seq_idx]  # out: bs * sent len, bs * sent len
                 bert_outputs = self.bert_pretrained.bert(t_i, t_m)
