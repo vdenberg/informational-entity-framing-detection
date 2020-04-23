e59a82c7bbbb9350f60c4c60b88ce78cd566c2d9
vdberg <vdberg@cl.uni-heidelberg.de>
2020-04-23 14:10:28 +0200

    try to get f above 0

--- experiments/ssc/scripts/train.sh
+++ experiments/ssc/scripts/train.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-#export SEED=15270
-export SEED=$1
+export SEED=15270
 export PYTORCH_SEED=`expr $SEED / 10`
 export NUMPY_SEED=`expr $PYTORCH_SEED / 10`
 
 # path to bert vocab and weights
-#export BERT_VOCAB=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/allennlp_files/scivocab_uncased.vocab
-#export BERT_WEIGHTS=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/allennlp_files/scibert_scivocab_uncased.tar.gz
-export BERT_VOCAB=bert-base-cased #uncased_L-12_H-768_A-12/vocab.txt
-export BERT_WEIGHTS=bert-base-cased #uncased_L-12_H-768_A-12/bert_model.ckpt.data-00000-of-00001
+if [ "$1" = 'SCIBERT' ]; then
+  export BERT_VOCAB=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/allennlp_files/scivocab_uncased.vocab
+  export BERT_WEIGHTS=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/allennlp_files/scibert_scivocab_uncased.tar.gz
+fi
+
+if [ "$1" = 'BERT' ]; then
+  export BERT_VOCAB=bert-base-cased #uncased_L-12_H-768_A-12/vocab.txt
+  export BERT_WEIGHTS=bert-base-cased #uncased_L-12_H-768_A-12/bert_model.ckpt.data-00000-of-00001
+fi
 
 # path to dataset files
 #export TRAIN_PATH=data/CSAbstruct/train.jsonl
 #export DEV_PATH=data/CSAbstruct/dev.jsonl
 #export TEST_PATH=data/CSAbstruct/test.jsonl
-export TRAIN_PATH="data/basil/"$2"_train_ssc.jsonl"
-export DEV_PATH="data/basil/"$2"_dev_ssc.jsonl"
-export TEST_PATH="data/basil/"$2"_test_ssc.jsonl"
+export TRAIN_PATH=data/1_train_ssc.jsonl
+export DEV_PATH=data/1_dev_ssc.jsonl
+export TEST_PATH=data/1_test_ssc.jsonl
 
 # model
 export USE_SEP=true  # true for our model. false for baseline
@@ -25,21 +29,22 @@ export WITH_CRF=false  # CRF only works for the baseline
 
 # training params
 #export cuda_device=0
-export cuda_device=0
+export cuda_device="$2"
 
 #export BATCH_SIZE=4
 #export LR=5e-5
 #export TRAINING_DATA_INSTANCES=1668
 #export NUM_EPOCHS=2
 
-export BATCH_SIZE=16
-export LR=0.0005
+#export cuda_device=0
+export BATCH_SIZE=1
+export LR=0.0008
 export TRAINING_DATA_INSTANCES=248
-export NUM_EPOCHS=5
+export NUM_EPOCHS=20
 
 # limit number of sentneces per examples, and number of words per sentence. This is dataset dependant
-export MAX_SENT_PER_EXAMPLE=1
-export SENT_MAX_LEN=76
+export MAX_SENT_PER_EXAMPLE=10
+export SENT_MAX_LEN=90
 #export MAX_SENT_PER_EXAMPLE=5
 #export SENT_MAX_LEN=20
 
