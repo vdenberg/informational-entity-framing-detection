8bbc5021be6f0808a6243fb9cd5f2d74fb875630
vdberg <vdberg@cl.uni-heidelberg.de>
2020-05-01 12:56:09 +0200

    debugging tok based clf

--- lib/evaluate/Eval.py
+++ lib/evaluate/Eval.py
@@ -6,18 +6,18 @@ import pandas as pd
 def get_metrics(labels, preds):
     assert len(preds) == len(labels)
 
+    nr_labels = set(labels)
+    if len(nr_labels) > 2:
+        preds = [2 if lab == 3 else lab for lab in preds]
+        labels = [2 if lab == 3 else lab for lab in labels]
+
     #mcc = matthews_corrcoef(labels, preds)
     acc = accuracy_score(labels, preds)
     prec_rec_fscore = precision_recall_fscore_support(labels, preds, labels=[0, 1])
     prec, rec, _ = [el[1] for el in prec_rec_fscore[:-1]]
 
-    nr_labels = set(labels)
-    average = 'binary' if len(nr_labels) == 2 else 'macro'
-    f1 = f1_score(labels, preds, average=average)
-
+    f1 = f1_score(labels, preds, average='binary')
     tn, fp, fn, tp = confusion_matrix(labels, preds).ravel()
-
-    #"mcc": mcc
     return {
         "tp": tp,
         "tn": tn,
