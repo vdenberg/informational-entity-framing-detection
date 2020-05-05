954f42f1aac60f25b453efbca183f10b0652adbc
vdberg <vdberg@cl.uni-heidelberg.de>
2020-05-05 14:31:24 +0200

    merge

--- experiments/sent_clf/ssc/scripts/train.sh
+++ experiments/sent_clf/ssc/scripts/train.sh
@@ -8,8 +8,11 @@ export NUMPY_SEED=`expr $PYTORCH_SEED / 10`
 #export BERT_VOCAB=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/allennlp_files/scivocab_uncased.vocab#
 #export BERT_WEIGHTS=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/allennlp_files/scibert_scivocab_uncased.tar.gz
 
-export BERT_VOCAB=bert-base-cased #uncased_L-12_H-768_A-12/vocab.txt
-export BERT_WEIGHTS=bert-base-cased #uncased_L-12_H-768_A-12/bert_model.ckpt.data-00000-of-00001
+#export BERT_VOCAB=bert-base-cased #uncased_L-12_H-768_A-12/vocab.txt
+#export BERT_WEIGHTS=bert-base-cased #uncased_L-12_H-768_A-12/bert_model.ckpt.data-00000-of-00001
+
+export BERT_VOCAB=https://huggingface.co/allenai/news_roberta_base
+export BERT_WEIGHTS=https://huggingface.co/allenai/news_roberta_base
 
 # path to dataset files
 #export TRAIN_PATH=data/CSAbstruct/train.jsonl
