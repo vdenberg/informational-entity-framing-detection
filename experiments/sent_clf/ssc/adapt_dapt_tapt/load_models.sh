d5ea4fc37a5d60b2b23d9f246881acb2de19f2ea
vdberg <vdberg@cl.uni-heidelberg.de>
2020-05-05 15:30:01 +0200

    enabling use of dapt and tapt

--- experiments/sent_clf/ssc/adapt_dapt_tapt/load_models.sh
+++ experiments/sent_clf/ssc/adapt_dapt_tapt/load_models.sh
@@ -3,11 +3,11 @@ python -m download_model \
         --serialization_dir $(pwd)/../pretrained_models/dsp_roberta_base_dapt_news_tapt_hyperpartisan_news_5015
 
 python -m  download_model \
-        --model allenai/dsp_roberta_base_tapt_hyperpartisan_news_5015 \
+        --model https://huggingface.co/allenai/dsp_roberta_base_tapt_hyperpartisan_news_5015 \
         --serialization_dir $(pwd)/../pretrained_models/dsp_roberta_base_tapt_hyperpartisan_news_5015
 
 python -m download_model \
-        --model allenai/news_roberta_base \
+        --model https://huggingface.co/allenai/news_roberta_base \
         --serialization_dir $(pwd)/../pretrained_models/news_roberta_base
 
 python convert_vocab.py
