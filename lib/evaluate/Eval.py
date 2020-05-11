d729fc913f60f353cd3a7f353cc22220544b92ab
vdberg <vdberg@cl.uni-heidelberg.de>
2020-05-11 19:01:23 +0200

    roberta that works

--- lib/evaluate/Eval.py
+++ lib/evaluate/Eval.py
@@ -18,7 +18,10 @@ def get_metrics(labels, preds):
         preds = convert_bio_to_binary(preds)
         labels = convert_bio_to_binary(labels)
 
-    print(set(labels))
+    else:
+        preds = [0 if lab == 1 else 1 for lab in preds]
+        labels = [0 if lab == 1 else 1 for lab in labels]
+
     assert set(labels) == {0, 1}
 
     #mcc = matthews_corrcoef(labels, preds)
