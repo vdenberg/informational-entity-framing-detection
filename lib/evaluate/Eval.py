7a1827cf799a3d55d31c33f7acb2777d780280f4
vdberg <vdberg@cl.uni-heidelberg.de>
2020-05-11 20:44:14 +0200

    roberta that works

--- lib/evaluate/Eval.py
+++ lib/evaluate/Eval.py
@@ -19,6 +19,8 @@ def get_metrics(labels, preds):
         labels = convert_bio_to_binary(labels)
 
     else:
+        print(preds)
+        print(labels)
         labels = [0 if lab == 1 else 1 for lab in labels]
 
     assert set(labels) == {0, 1}
