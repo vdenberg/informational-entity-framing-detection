d2c732bc3c12c4c6dc5699c16de4ecbe6113a6e3
vdberg <vdberg@cl.uni-heidelberg.de>
2020-05-12 17:17:01 +0200

    ssc roberta

--- experiments/sent_clf/ssc/scripts/train.sh
+++ experiments/sent_clf/ssc/scripts/train.sh
@@ -9,9 +9,9 @@ export BERT_VOCAB=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/all
 export BERT_WEIGHTS=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/allennlp_files/scibert_scivocab_uncased.tar.gz
 
 # path to dataset files
-export TRAIN_PATH=data/CSAbstruct/train.jsonl
-export DEV_PATH=data/CSAbstruct/dev.jsonl
-export TEST_PATH=data/CSAbstruct/test.jsonl
+export TRAIN_PATH=data/basil/2_train_ssc.jsonl
+export DEV_PATH=data/basil/2_dev_ssc.jsonl
+export TEST_PATH=data/basil/2_test_ssc.jsonl
 
 # model
 export USE_SEP=true  # true for our model. false for baseline
@@ -21,12 +21,12 @@ export WITH_CRF=false  # CRF only works for the baseline
 export cuda_device=0
 export BATCH_SIZE=4
 export LR=5e-5
-export TRAINING_DATA_INSTANCES=1668
+export TRAINING_DATA_INSTANCES=248
 export NUM_EPOCHS=2
 
 # limit number of sentneces per examples, and number of words per sentence. This is dataset dependant
-export MAX_SENT_PER_EXAMPLE=10
-export SENT_MAX_LEN=80
+export MAX_SENT_PER_EXAMPLE=5
+export SENT_MAX_LEN=90
 
 # this is for the evaluation of the summarization dataset
 export SCI_SUM=false
@@ -34,5 +34,9 @@ export USE_ABSTRACT_SCORES=false
 export SCI_SUM_FAKE_SCORES=false  # use fake scores for testing
 
 CONFIG_FILE=sequential_sentence_classification/config.jsonnet
+
+if [ -d "tmp_output_dir" ]; then
+  rm -r tmp_output_dir
+fi
 
 python -m allennlp.run train $CONFIG_FILE  --include-package sequential_sentence_classification -s $SERIALIZATION_DIR "$@"
