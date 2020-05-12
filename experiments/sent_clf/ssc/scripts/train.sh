c357c969001b015d72c26bad883323d19fe35fad
vdberg <vdberg@cl.uni-heidelberg.de>
2020-05-12 16:47:35 +0200

    ssc roberta

--- experiments/sent_clf/ssc/scripts/train.sh
+++ experiments/sent_clf/ssc/scripts/train.sh
@@ -5,7 +5,7 @@ export PYTORCH_SEED=`expr $SEED / 10`
 export NUMPY_SEED=`expr $PYTORCH_SEED / 10`
 
 # path to bert vocab and weights
-export BERT_VOCAB=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/allennlp_files/scivocab_uncased.vocab#
+export BERT_VOCAB=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/allennlp_files/scivocab_uncased.vocab
 export BERT_WEIGHTS=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/allennlp_files/scibert_scivocab_uncased.tar.gz
 
 #export BERT_VOCAB=bert-base-cased #uncased_L-12_H-768_A-12/vocab.txt
