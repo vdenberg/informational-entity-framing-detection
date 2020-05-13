16bee06269b56465f026a755e57232093aedbf09
vdberg <vdberg@cl.uni-heidelberg.de>
2020-05-13 14:09:09 +0200

    ssc roberta

--- experiments/sent_clf/ssc/sequential_sentence_classification/dataset_reader.py
+++ experiments/sent_clf/ssc/sequential_sentence_classification/dataset_reader.py
@@ -42,7 +42,7 @@ class SeqClassificationReader(DatasetReader):
                  predict: bool = False,
                  ) -> None:
         super().__init__(lazy)
-        self._tokenizer = PretrainedTransformerTokenizer(transformer_model_name)
+        self._tokenizer = PretrainedTransformerTokenizer(transformer_model_name, do_lowercase=False)
         self._token_indexers = {"tokens": PretrainedTransformerIndexer(transformer_model_name)}
         self.sent_max_len = sent_max_len
         self.use_sep = use_sep
