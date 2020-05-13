5edca1f0646048476a0a1931b5fdd0a48d8e8e03
vdberg <vdberg@cl.uni-heidelberg.de>
2020-05-13 14:33:19 +0200

    ssc roberta

--- experiments/sent_clf/ssc/sequential_sentence_classification/model.py
+++ experiments/sent_clf/ssc/sequential_sentence_classification/model.py
@@ -60,10 +60,8 @@ class SeqClassificationModel(Model):
                 label_name = self.vocab.get_token_from_index(namespace='labels', index=label_index)
                 self.label_f1_metrics[label_name] = F1Measure(label_index)
 
-        print(vars(self.text_field_embedder))
-        print(self.text_field_embedder._token_embedders)
-        exit(0)
-        encoded_sentence_dim = self.text_field_embedder._token_embedders['bert'].output_dim
+        #encoded_sentence_dim = self.text_field_embedder._token_embedders['bert'].output_dim
+        encoded_sentence_dim = self.text_field_embedder.get_output_dim()
 
         ff_in_dim = encoded_sentence_dim if self.use_sep else self_attn.get_output_dim()
         ff_in_dim += self.additional_feature_size
