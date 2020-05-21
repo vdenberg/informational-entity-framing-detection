04501758a8e417ff41789cc4b836690ef67760a2
vdberg <vdberg@cl.uni-heidelberg.de>
2020-05-21 16:36:34 +0200

    reimplementing ssc with roberta

--- lib/classifiers/RobertaSSCWrapper.py
+++ lib/classifiers/RobertaSSCWrapper.py
@@ -348,6 +348,7 @@ class Inferencer():
             preds = np.reshape(preds, labels.shape)
         elif output_mode == 'ssc':
             labels = labels.numpy().flatten()
+            labels = [l for l in labels if l != -1]
         else:
             labels = labels.numpy()
 
