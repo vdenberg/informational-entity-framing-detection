0da47659ec1027633e32b9c9eedec540ac58edf7
vdberg <vdberg@cl.uni-heidelberg.de>
2020-05-21 16:41:17 +0200

    reimplementing ssc with roberta

--- lib/evaluate/Eval.py
+++ lib/evaluate/Eval.py
@@ -13,7 +13,6 @@ def convert_bio_to_binary(labels):
 
 def get_metrics(labels, preds):
     assert len(preds) == len(labels)
-    print(labels)
 
     if len(set(labels)) > 2:
         preds = convert_bio_to_binary(preds)
