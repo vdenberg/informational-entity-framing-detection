9c1b3c82a916221174724a0fb3eb85726712d42d
vdberg <vdberg@cl.uni-heidelberg.de>
2020-05-28 16:11:43 +0200

    experiments to find out why ssc does not work

--- experiments/sent_clf/ssc/ssc_w_roberta/roberta_ssc_pytorch_finetune.py
+++ experiments/sent_clf/ssc/ssc_w_roberta/roberta_ssc_pytorch_finetune.py
@@ -46,7 +46,7 @@ args = parser.parse_args()
 seeds = [args.sv] if args.sv else [0]
 bss = [args.bs] if args.bs else [16]
 lrs = [args.lr] if args.lr else [2e-5]
-folds = [args.fold] if args.fold else ['2']
+folds = [args.fold] if args.fold else ['2', '5']
 
 model_mapping = {'rob_base': 'roberta-base',
                  'rob_dapt': 'experiments/adapt_dapt_tapt/pretrained_models/news_roberta_base',
