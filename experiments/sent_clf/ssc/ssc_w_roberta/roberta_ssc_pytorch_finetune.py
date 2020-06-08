c3d29cd128cb7f088952fc5d02a27ffd21dfa308
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-08 18:26:07 +0200

    experiments to find out why ssc does not work

--- experiments/sent_clf/ssc/ssc_w_roberta/roberta_ssc_pytorch_finetune.py
+++ experiments/sent_clf/ssc/ssc_w_roberta/roberta_ssc_pytorch_finetune.py
@@ -47,7 +47,7 @@ args = parser.parse_args()
 #ssc5: 49_bs16_lr3e-05_f2
 #ssc4: 49_bs16_lr0.0002_f1
 #ssc3: 49_bs16_lr1e-05_f3
-seeds = [args.sv] if args.sv else [49, 181] # 34, 49, 181
+seeds = [args.sv] if args.sv else [34, 49, 181] # 34, 49, 181
 bss = [args.bs] if args.bs else [10, 6] # 16, 10
 lrs = [args.lr] if args.lr else [2e-5, 3e-5, 5e-5] #
 folds = [args.fold] if args.fold else ['1', '2', '3', '4', '5']
