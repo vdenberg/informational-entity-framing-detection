a031f561b40f7d5d14ae75b5d8b50bcf446f4bd1
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-09 14:59:12 +0200

    experiments to find out why ssc does not work

--- experiments/sent_clf/ssc/ssc_w_roberta/roberta_ssc_pytorch_finetune.py
+++ experiments/sent_clf/ssc/ssc_w_roberta/roberta_ssc_pytorch_finetune.py
@@ -49,8 +49,8 @@ args = parser.parse_args()
 #ssc3: 49_bs16_lr1e-05_f3
 models = [args.model] if args.model else ['rob_base', 'rob_dapt', 'rob_tapt', 'rob_dapttapt']
 seeds = [args.sv] if args.sv else [34, 49, 181] # 34, 49, 181
-bss = [args.bs] if args.bs else [8, 6] # 16, 10
-lrs = [args.lr] if args.lr else [1e-5, 1.5e-5, 2e-5] #
+bss = [args.bs] if args.bs else [6] # 16, 10
+lrs = [args.lr] if args.lr else [1e-5] #
 folds = [args.fold] if args.fold else ['1', '2', '3', '4', '5']
 samplers = [args.sampler] if args.sampler else ['sequential', 'random']
 N_EPS = args.n_epochs
