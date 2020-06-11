9cfbae9203ab067596d770634c412c07d023caf2
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-11 11:51:40 +0200

    experiments to find out why ssc does not work

--- experiments/sent_clf/baselines/roberta_pytorch_finetune.py
+++ experiments/sent_clf/baselines/roberta_pytorch_finetune.py
@@ -231,9 +231,9 @@ if __name__ == '__main__':
                                 all_ids, all_batches, all_labels = load_features('data/features_for_bert/all_features.pkl', batch_size=1, sampler=SAMPLER)
                                 embs = inferencer.predict(model, all_batches, return_embeddings=True, emb_type=EMB_TYPE)
                                 basil_w_BERT = pd.DataFrame(index=all_ids)
-                                basil_w_BERT[EMB_TYPE] = embs
                                 print(len(embs), len(embs[0]))
                                 print(embs.shape)
+                                basil_w_BERT[EMB_TYPE] = embs
                                 emb_name = f'{name}_basil_w_{EMB_TYPE}'
                                 basil_w_BERT.to_csv(f'data/{emb_name}.csv')
                                 logger.info(f'Written embs ({len(embs)},{len(embs[0])}) to data/{emb_name}.csv')
