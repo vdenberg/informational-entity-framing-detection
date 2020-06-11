6467af5ae2425d0cfedde05b9602287ce05918eb
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-11 15:19:08 +0200

    experiments to find out why ssc does not work

--- experiments/sent_clf/cam/cam_experiment.py
+++ experiments/sent_clf/cam/cam_experiment.py
@@ -478,7 +478,9 @@ for HIDDEN in hiddens:
                 setting_name = base_name + f"_{SEED_VAL}" + h_name + bs_name + lr_name
                 setting_table_fp = f'{TABLE_DIR}/{setting_name}.csv'
                 logger.info(f' Setting table in: {setting_table_fp}.')
-                if os.path.exists(setting_table_fp):
+
+                FORCE = True
+                if os.path.exists(setting_table_fp) or FORCE:
                     logger.info(f'Setting {setting_name} done already.')
                     setting_results_table = pd.read_csv(setting_table_fp, index_col=None)
 
