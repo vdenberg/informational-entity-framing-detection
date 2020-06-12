a6dbb2410f8dde4edb3e34716cd52f853cb07769
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-12 17:51:16 +0200

    cam restart

--- lib/handle_data/SplitData.py
+++ lib/handle_data/SplitData.py
@@ -293,9 +293,9 @@ class Split:
                     self.input_dataframe.rename({'bias': 'label'})
 
             # oversample
-            pos_cases = self.input_dataframe[self.input_dataframe.label == 1]
-            pos_cases = pd.concat([pos_cases]*5)
-            self.input_dataframe = pd.concat([self.input_dataframe, pos_cases])
+            #pos_cases = self.input_dataframe[self.input_dataframe.label == 1]
+            #pos_cases = pd.concat([pos_cases]*5)
+            #self.input_dataframe = pd.concat([self.input_dataframe, pos_cases])
 
             train_df = self.input_dataframe.loc[train_sent_ids, :]
             train_df = self.input_dataframe.loc[train_sent_ids, features + ['label']]
