d0cc896e5d40f9b71a03f00dae55d319238c4e22
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-12 16:06:19 +0200

    cam restart

--- experiments/sent_clf/cam/cam_experiment.py
+++ experiments/sent_clf/cam/cam_experiment.py
@@ -418,8 +418,8 @@ if EMB_TYPE in ['use', 'sbert']:
 for fold in folds:
     # read embeddings file
     if EMB_TYPE in ['poolbert', 'avbert']:
-        #embed_fp = f"data/bert_231_bs16_lr2e-05_f{fold['name']}_basil_w_{EMB_TYPE}.csv"
-        embed_fp = f"data/rob_base_sequential_34_bs16_lr1e-05_f{fold['name']}_basil_w_{EMB_TYPE}.csv"
+        embed_fp = f"data/bert_231_bs16_lr2e-05_f{fold['name']}_basil_w_{EMB_TYPE}.csv"
+        #embed_fp = f"data/rob_base_sequential_34_bs16_lr1e-05_f{fold['name']}_basil_w_{EMB_TYPE}.csv"
         weights_matrix = get_weights_matrix(data, embed_fp, emb_dim=EMB_DIM)
         logger.info(f" --> Loaded from {embed_fp}, shape: {weights_matrix.shape}")
     fold['weights_matrix'] = weights_matrix
