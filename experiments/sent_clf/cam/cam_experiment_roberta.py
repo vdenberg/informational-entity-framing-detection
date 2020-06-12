ceaa8a43738318e8e0e18003f7a233feda9e7bfd
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-12 13:59:11 +0200

    cam restart

--- experiments/sent_clf/cam/cam_experiment_roberta.py
+++ experiments/sent_clf/cam/cam_experiment_roberta.py
@@ -97,8 +97,7 @@ def get_weights_matrix(data, emb_fp, emb_dim=None):
     print(b[:20])
     print(len(a), len(b))
     c = set(a) - set(b)
-    print(list(c))
-    print(len(c))
+
     exit()
     data_w_emb = data_w_emb.loc[data.sentence_ids]
     wm = make_weight_matrix(data_w_emb, emb_dim)
