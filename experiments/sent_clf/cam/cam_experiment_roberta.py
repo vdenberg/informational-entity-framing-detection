2ca11e19dd2292c5a883b145aee78dad5ebafec7
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-12 14:16:57 +0200

    cam restart

--- experiments/sent_clf/cam/cam_experiment_roberta.py
+++ experiments/sent_clf/cam/cam_experiment_roberta.py
@@ -92,7 +92,7 @@ def get_weights_matrix(data, emb_fp, emb_dim=None):
     data_w_emb.index = [standardise_id(x) for x in data_w_emb.index]
     tmp = len(data), len(data_w_emb)
     tmp = [el for el in data.index if el not in data_w_emb.index]
-    print(tmp)
+    print(emb_fp)
     data_w_emb = data_w_emb.loc[data.sentence_ids]
     wm = make_weight_matrix(data_w_emb, emb_dim)
     return wm
