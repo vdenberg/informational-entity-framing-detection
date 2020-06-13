9e33e9920bd11e9671571e3a372e2a44c408a5da
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-13 17:08:42 +0200

    cam restart

--- lib/classifiers/ContextAwareClassifier.py
+++ lib/classifiers/ContextAwareClassifier.py
@@ -170,8 +170,7 @@ class ContextAwareClassifier():
 
         self.model.zero_grad()
         logits, probs, _ = self.model(inputs)
-        # loss = self.criterion(logits.view(-1, 2), labels.view(-1))
-        loss = self.criterion(torch.log(logits.view(-1, 2)), labels.view(-1))
+        loss = self.criterion(logits.view(-1, 2), labels.view(-1))
         loss.backward()
 
         self.optimizer.step()
