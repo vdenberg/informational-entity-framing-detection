334a574d82df8454e9178724f1bd2d9e7c99cf8d
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-15 15:38:20 +0200

    cam restart

--- experiments/sent_clf/cam/cam_experiment.py
+++ experiments/sent_clf/cam/cam_experiment.py
@@ -268,7 +268,7 @@ if PREPROCESS:
 
     raw_data['source'] = sentences['source']
     print(sentences['source'])
-    print(sentences['source'].isna())
+    print(sentences[sentences['source'].isna()])
     print(raw_data['source'])
     print(raw_data['source'].isna())
     raw_data['src_num'] = raw_data.source.apply(lambda x: {'fox': 0, 'nyt': 1, 'hpo': 2}[x])
