0c3e2ac8e7e8412c81df403ae8443e042e59c82c
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-15 15:41:45 +0200

    cam restart

--- experiments/sent_clf/cam/cam_experiment.py
+++ experiments/sent_clf/cam/cam_experiment.py
@@ -264,12 +264,12 @@ if PREPROCESS:
     raw_data = pd.read_csv(raw_data_fp, sep='\t',
                            names=['sentence_ids', 'context_document', 'label', 'position'],
                            dtype={'sentence_ids': str, 'tokens': str, 'label': int, 'position': int}, index_col=False)
-    sentences = sentences.set_index('sentence_ids', drop=False)
     raw_data = raw_data.set_index('sentence_ids', drop=False)
 
+    tmp = set(sentences.index) - set(raw_data.index)
+    print(tmp)
+
     raw_data['source'] = sentences['source']
-    print(sentences[sentences.source.isna()].source)
-    print(raw_data[raw_data.source.isna()].source)
     raw_data['src_num'] = raw_data.source.apply(lambda x: {'fox': 0, 'nyt': 1, 'hpo': 2}[x])
     raw_data['story'] = sentences['story']
     raw_data['sentence'] = sentences['sentence']
