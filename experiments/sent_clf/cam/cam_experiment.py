17d0baef6733cc4e4e66bb0c55bb119dc837516f
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-15 15:49:04 +0200

    cam restart

--- experiments/sent_clf/cam/cam_experiment.py
+++ experiments/sent_clf/cam/cam_experiment.py
@@ -261,6 +261,8 @@ if PREPROCESS:
 
     sentences = pd.read_csv('data/basil.csv', index_col=0).fillna('')
     sentences.index = [el.lower() for el in sentences.index]
+    sentences.source = [el.lower() for el in sentences.source]
+
     raw_data_fp = os.path.join(DATA_DIR, 'merged_basil.tsv')
     raw_data = pd.read_csv(raw_data_fp, sep='\t',
                            names=['sentence_ids', 'context_document', 'label', 'position'],
