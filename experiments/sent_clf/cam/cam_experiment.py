a5b669fe44302f68565aa6f6ab38e6868023d62b
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-15 15:42:28 +0200

    cam restart

--- experiments/sent_clf/cam/cam_experiment.py
+++ experiments/sent_clf/cam/cam_experiment.py
@@ -260,14 +260,12 @@ if PREPROCESS:
     logger.info(f" Max doc len: {MAX_DOC_LEN}")
 
     sentences = pd.read_csv('data/basil.csv', index_col=0).fillna('')
+    sentences.index = sentences.index.lower()
     raw_data_fp = os.path.join(DATA_DIR, 'merged_basil.tsv')
     raw_data = pd.read_csv(raw_data_fp, sep='\t',
                            names=['sentence_ids', 'context_document', 'label', 'position'],
                            dtype={'sentence_ids': str, 'tokens': str, 'label': int, 'position': int}, index_col=False)
     raw_data = raw_data.set_index('sentence_ids', drop=False)
-
-    tmp = set(sentences.index) - set(raw_data.index)
-    print(tmp)
 
     raw_data['source'] = sentences['source']
     raw_data['src_num'] = raw_data.source.apply(lambda x: {'fox': 0, 'nyt': 1, 'hpo': 2}[x])
