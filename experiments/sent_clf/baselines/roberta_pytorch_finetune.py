f17caf2954fec081c8adc6418d992502a12a5c10
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-16 23:34:21 +0200

    cam restart

--- experiments/sent_clf/baselines/roberta_pytorch_finetune.py
+++ experiments/sent_clf/baselines/roberta_pytorch_finetune.py
@@ -242,7 +242,7 @@ if __name__ == '__main__':
                                 emb_fp = f'data/{name}_basil_w_{EMB_TYPE}'
 
                                 if not os.path.exists(emb_fp):
-                                    logging.info(f'Generating {EMB_TYPE} embeds')
+                                    logging.info(f'Generating {EMB_TYPE} embeds ({emb_fp})')
                                     feat_fp = os.path.join(FEAT_DIR, f"all_features.pkl")
                                     all_ids, all_batches, all_labels = load_features(feat_fp, batch_size=1, sampler=SAMPLER)
                                     embs = inferencer.predict(best_model, all_batches, return_embeddings=True, emb_type=EMB_TYPE)
