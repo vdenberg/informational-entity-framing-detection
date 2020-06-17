637161a3107d3a42c3230d0019004e46cf74f86e
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-17 21:44:17 +0200

    cam restart

--- experiments/sent_clf/cam/cam_experiment.py
+++ experiments/sent_clf/cam/cam_experiment.py
@@ -120,7 +120,7 @@ parser.add_argument('-inf', '--step_info_every', type=int, default=250)
 parser.add_argument('-cp', '--save_epoch_cp_every', type=int, default=50)
 
 # DATA PARAMS
-parser.add_argument('-spl', '--split_type', help='Options: fan|berg|both',type=str, default='s')
+parser.add_argument('-spl', '--split_type', help='Options: fan|berg|both',type=str, default='both')
 parser.add_argument('-subset', '--subset_of_data', type=float, help='Section of data to experiment on', default=1.0)
 parser.add_argument('-pp', '--preprocess', action='store_true', default=False, help='Whether to proprocess again')
 
