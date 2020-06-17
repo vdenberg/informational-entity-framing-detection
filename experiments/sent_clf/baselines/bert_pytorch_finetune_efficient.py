da17a1e573534f4557871c955f8876f84a336c9d
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-17 14:54:56 +0200

    cam restart

--- experiments/sent_clf/baselines/bert_pytorch_finetune_efficient.py
+++ experiments/sent_clf/baselines/bert_pytorch_finetune_efficient.py
@@ -63,8 +63,8 @@ N_EPS = args.n_epochs
 models = [args.model] if args.model else ['bert']
 seeds = [args.sv] if args.sv else [49, 6, 34]
 bss = [args.bs] if args.bs else [16]
-lrs = [args.lr] if args.lr else [1e-5]
-folds = [args.fold] if args.fold else [str(el+1) for el in range(10)]
+lrs = [args.lr] if args.lr else [2e-5]
+folds = [args.fold] if args.fold else ['fan'] + [str(el+1) for el in range(10)]
 samplers = [args.sampler] if args.sampler else ['sequential']
 
 DEBUG = args.debug
