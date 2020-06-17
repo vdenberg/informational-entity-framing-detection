4486f861f06d85572241c4d3598a34f8360a0abd
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-17 17:16:13 +0200

    cam restart

--- create_data/preprocess_for_roberta_ssc.py
+++ create_data/preprocess_for_roberta_ssc.py
@@ -127,7 +127,9 @@ def redistribute_feats(features, cls=0, pad=1, max_sent=10, max_len=None):
             if i != nr_sequences-1:
                 winend = sequences[i+1][0:window_size]
                 winseq = winseq + winend
+            print(winseq)
             sequence_rows.append(winseq)
+    exit(0)
 
     # help measure what the maxlen should be
     for row in sequence_rows:
