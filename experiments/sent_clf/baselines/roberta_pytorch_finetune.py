dcb41448b2f9682a06ba8bbe0fd8150b403562f6
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-17 14:31:33 +0200

    cam restart

--- experiments/sent_clf/baselines/roberta_pytorch_finetune.py
+++ experiments/sent_clf/baselines/roberta_pytorch_finetune.py
@@ -57,8 +57,8 @@ parser.add_argument('-fold', '--fold', type=str, default=None) #16, 21
 args = parser.parse_args()
 
 N_EPS = args.n_epochs
-models = [args.model] if args.model else ['rob_base']
-seeds = [args.sv] if args.sv else [34]
+models = [args.model] if args.model else ['rob_dapt', 'rob_tapt', 'rob_dapttapt']
+seeds = [args.sv] if args.sv else [49, 6, 34]
 bss = [args.bs] if args.bs else [16]
 lrs = [args.lr] if args.lr else [1e-5]
 folds = [args.fold] if args.fold else [str(el+1) for el in range(10)]
