476cb4813c33b59b7347576df429bc019db92810
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-19 12:42:43 +0200

    cam compare

--- lib/classifiers/ContextAwareClassifier.py
+++ lib/classifiers/ContextAwareClassifier.py
@@ -112,7 +112,7 @@ class ContextAwareModel(nn.Module):
                 # target_sent_reps[item] = target_hid
 
             # heavy_context_rep = torch.cat((target_sent_reps, final_sent_reps, embedded_pos, embedded_src), dim=-1)
-            context_rep = torch.cat((target_sent_reps, final_sent_reps, embedded_src), dim=-1)
+            context_rep = torch.cat((target_sent_reps, final_sent_reps), dim=-1)
             # target_sent_reps = torch.cat((target_sent_reps, final_sent_reps), dim=-1)
             target_sent_reps = context_rep
 
