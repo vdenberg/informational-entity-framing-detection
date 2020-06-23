9e0c53294b0ab5fcf0cd70374df5ebdf941e99d9
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-23 14:40:48 +0200

    error analysis

--- lib/classifiers/ContextAwareClassifier.py
+++ lib/classifiers/ContextAwareClassifier.py
@@ -35,12 +35,12 @@ class ContextAwareModel(nn.Module):
         self.bilstm_layers = bilstm_layers
         self.device = device
 
-        self.weights_matrix = None #torch.tensor(weights_matrix, dtype=torch.float, device=self.device)
-        self.embedding = None #Embedding.from_pretrained(self.weights_matrix)
+        self.weights_matrix = torch.tensor(weights_matrix, dtype=torch.float, device=self.device)
+        self.embedding = Embedding.from_pretrained(self.weights_matrix)
         self.embedding_pos = Embedding(pos_quartiles, pos_dim) # 4=nr of quart
         self.embedding_src = Embedding(nr_srcs, src_dim)
 
-        self.emb_size = None #weights_matrix.shape[1]
+        self.emb_size = weights_matrix.shape[1]
 
         self.lstm = LSTM(self.input_size, self.hidden_size, num_layers=self.bilstm_layers, bidirectional=True, dropout=0.2)
         self.dropout = Dropout(0.6)
