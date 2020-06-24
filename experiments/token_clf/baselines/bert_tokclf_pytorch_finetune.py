af025e08b420ec12bfd0ed54cdd5ae2a66e81ec9
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-24 22:07:44 +0200

    more seeds

--- experiments/token_clf/baselines/bert_tokclf_pytorch_finetune.py
+++ experiments/token_clf/baselines/bert_tokclf_pytorch_finetune.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
                   }
     '''
 
-    for SEED in [132, 23, 281, 132, 281]: #132, 281,  #tok_clf: [132, 281, 45362], sent_clf: 231 (redo this one, tokclf overwrote some), 26354, 182,
+    for SEED in [132, 23, 281, 132, 45362]: #132, 281,  #tok_clf: [132, 281, 45362], sent_clf: 231 (redo this one, tokclf overwrote some), 26354, 182,
         if SEED == 0:
             SEED_VAL = random.randint(0, 300)
         else:
@@ -105,9 +105,9 @@ if __name__ == '__main__':
         torch.manual_seed(SEED_VAL)
         torch.cuda.manual_seed_all(SEED_VAL)
 
-        for BATCH_SIZE in [16]:
+        for BATCH_SIZE in [16, 24]:
             bs_name = seed_name + f"_bs{BATCH_SIZE}"
-            for LEARNING_RATE in [1e-5, 5e-6]: #2e-5, 3e-5], 5e-5]:
+            for LEARNING_RATE in [2e-5]: #2e-5, 3e-5], 5e-5]:
                 setting_name = bs_name + f"_lr{LEARNING_RATE}"
                 setting_results_table = pd.DataFrame(columns=table_columns.split(','))
                 for fold_name in ['fan', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
