df6da0a86adabfbcfaeabfe3afbaa1045b1472de
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-25 20:48:13 +0200

    winssc

--- experiments/sent_clf/ssc/ssc_w_roberta/roberta_ssc_pytorch_finetune_efficient.py
+++ experiments/sent_clf/ssc/ssc_w_roberta/roberta_ssc_pytorch_finetune_efficient.py
@@ -83,7 +83,7 @@ if DEBUG:
 # WHERE ARE THE FILES
 ########################
 
-TASK_NAME = f'WSSC{EX_LEN}' if WINDOW else f'WSSC{EX_LEN}'
+TASK_NAME = f'WSSC{EX_LEN}' if WINDOW else f'SSC{EX_LEN}'
 FEAT_DIR = f'data/sent_clf/features_for_roberta_ssc/windowed/ssc{EX_LEN}' if WINDOW else f'data/sent_clf/features_for_roberta_ssc/ssc{EX_LEN}'
 CHECKPOINT_DIR = f'models/checkpoints/{TASK_NAME}/'
 CURRENT_BEST_DIR = f'models/checkpoints/{TASK_NAME}/current_best'
