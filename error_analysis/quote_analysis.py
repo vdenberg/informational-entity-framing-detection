82f210ec26750492ab5bb384c5f3506e844e5836
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-25 17:44:47 +0200

    winssc

--- error_analysis/quote_analysis.py
+++ error_analysis/quote_analysis.py
@@ -3,27 +3,29 @@ from lib.evaluate.Eval import my_eval
 
 
 def got_quote(x):
-    double_q = '"' in x
+    double_q = '"' in str(x)
     return double_q
 
-sentences = pd.read_csv('data/basil.csv', index_col=0).fillna('')
 source_df = pd.DataFrame(columns=['source', 'prec', 'rec', 'f1'])
+
 pd.set_option('display.max_columns', 10)
 pd.set_option('display.max_colwidth', 500)
 
+average_ratio = 0
+average_ratio_in_tp = 0
 for f in [str(el) for el in range(1,11)]:
     df = pd.read_csv(f"data/dev_w_preds/{f}_dev_w_pred.csv", index_col=0)
-    df['sentence'] = sentences.loc[df.index].sentence
+
     df['quote'] = df.sentence.apply(got_quote)
+    df['tp'] = (df.label == 1) & (df.pred == 1)
 
-    # ANALYZE BY SOURCE
-    tp = df[(df.label == 1) & (df.dev == 1)]
-    fp = df[(df.label == 0) & (df.dev == 1)]
-    tn = df[(df.label == 0) & (df.dev == 0)]
-    fn = df[(df.label == 1) & (df.dev == 0)]
+    df_tp = df[df.tp == True]
+    df_no_tp = df[df.tp == False]
 
-    for n, el in zip(['tp', 'fp', 'tn', 'fn'], [tp, fp, tn, fn]):
-        tot = len(el)
-        s = sum(el.quote)
-        print(n, s/tot)
-        print()
+    ratio = sum(df_no_tp.quote) / len(df_no_tp)
+    ratio_in_tp = sum(df_tp.quote) / len(df_tp)
+    average_ratio += ratio
+    average_ratio_in_tp += ratio_in_tp
+
+print(average_ratio / 10)
+print(average_ratio_in_tp / 10)
