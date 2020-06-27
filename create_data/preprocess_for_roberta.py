c41cf931b7e577da3b2928cfdf736469a9f68919
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-27 16:09:36 +0200

    tc

--- create_data/preprocess_for_roberta.py
+++ create_data/preprocess_for_roberta.py
@@ -66,7 +66,7 @@ FORCE = True
 if not os.path.exists(ofp) or FORCE:
     examples = dataloader.get_examples(all_infp, 'train', sep='\t')
     labels = [example.label for example in examples]
-    print(labels)
+    print(labels[:25])
     examples = [(example, label_map, MAX_SEQ_LENGTH, tokenizer, OUTPUT_MODE) for example in examples if example.text_a]
     features = preprocess(examples)
     features_dict = {feat.my_id: feat for feat in features}
