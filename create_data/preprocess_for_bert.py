04212bce1704cd3c02b2693f3b2c245bfad39e74
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-27 17:39:37 +0200

    tc

--- create_data/preprocess_for_bert.py
+++ create_data/preprocess_for_bert.py
@@ -53,7 +53,7 @@ else:
     label_map = {label: i for i, label in enumerate(label_list)}
 
 all_infp = os.path.join(DATA_DIR, f"all.tsv")
-ofp = os.path.join(FEAT_DIR, f"all_features.pkl")
+ofp = os.path.join(FEAT_DIR, f"all_features_test.pkl")
 
 FORCE = False
 if not os.path.exists(ofp) or FORCE:
