a6dd21a75310118a84652d88f8c16e19cce585d7
vdberg <vdberg@cl.uni-heidelberg.de>
2020-06-27 16:39:41 +0200

    tc

--- create_data/preprocess_for_roberta.py
+++ create_data/preprocess_for_roberta.py
@@ -64,11 +64,13 @@ ofp = os.path.join(FEAT_DIR, f"all_features.pkl")
 FORCE = True
 if not os.path.exists(ofp) or FORCE:
     examples = dataloader.get_examples(all_infp, 'train', sep='\t')
+    examples = [example for example in examples if example.label != '[]']
+
     examples = [(example, label_map, MAX_SEQ_LENGTH, tokenizer, OUTPUT_MODE) for example in examples if example.text_a]
 
-    examples = [example for example in examples if example.label != '[]']
     features = preprocess(examples)
     features_dict = {feat.my_id: feat for feat in features}
+
     print([feat.label_id for feat in features])
 
     print(f"Processed fold all - {len(features)} items")
