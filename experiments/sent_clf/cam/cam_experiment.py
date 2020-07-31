ef6b11f2b732585d487d0a7e261d6f1bfb3759b9
vdberg <vdberg@cl.uni-heidelberg.de>
2020-07-31 17:35:09 +0200

    adding raw data

--- experiments/sent_clf/cam/cam_experiment.py
+++ experiments/sent_clf/cam/cam_experiment.py
@@ -356,9 +356,9 @@ for fold in folds:
     # test_batches = to_batches(to_tensors(split=fold['test'], device=device), batch_size=BATCH_SIZE, sampler=SAMPLER)
 
 
-    fold['train_batches'] = [to_batches(to_tensors(split=voter['train'], device=device), batch_size=BATCH_SIZE, sampler=SAMPLER) for voter in fold]
-    fold['dev_batches'] = [to_batches(to_tensors(split=voter['dev'], device=device), batch_size=BATCH_SIZE, sampler=SAMPLER) for voter in fold]
-    fold['test_batches'] = [to_batches(to_tensors(split=voter['test'], device=device), batch_size=BATCH_SIZE, sampler=SAMPLER) for voter in fold]
+    fold['train_batches'] = [to_batches(to_tensors(split=voter, device=device), batch_size=BATCH_SIZE, sampler=SAMPLER) for voter in fold['train']]
+    fold['dev_batches'] = [to_batches(to_tensors(split=voter, device=device), batch_size=BATCH_SIZE, sampler=SAMPLER) for voter in fold['dev']]
+    fold['test_batches'] = [to_batches(to_tensors(split=voter, device=device), batch_size=BATCH_SIZE, sampler=SAMPLER) for voter in fold['test']]
 
 # =====================================================================================
 #                    LOAD EMBEDDINGS
