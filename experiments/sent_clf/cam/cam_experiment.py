3da513f18ab62cf1d224789b342ada9669df9253
vdberg <vdberg@cl.uni-heidelberg.de>
2020-08-02 14:16:15 +0200

    majority vote cross val

--- experiments/sent_clf/cam/cam_experiment.py
+++ experiments/sent_clf/cam/cam_experiment.py
@@ -493,7 +493,6 @@ for HIDDEN in hiddens:
                                 print(tmp)
 
                             maj_vote = [Counter(line).most_common()[0] for line in zip(*all_preds)]
-                            print(maj_vote)
                             test_mets, test_perf = my_eval(fold['test'].label, maj_vote, name='majority vote', set_type='test')
                             test_results.update(test_mets)
 
