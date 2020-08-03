9d33b2e69b8f05114ed0534327ff82e41110efc1
vdberg <vdberg@cl.uni-heidelberg.de>
2020-08-03 15:01:34 +0200

    majority vote cross val

--- lib/handle_data/SplitData.py
+++ lib/handle_data/SplitData.py
@@ -126,7 +126,7 @@ class BergSplit:
         self.split_input = split_input
         self.basil = load_basil().sample(frac=subset)
 
-    def create_split(self):
+    def create_split(self, n_voters):
         # order stories from most to least sentences in a story
         ordered_stories = order_stories(self.basil)
 
