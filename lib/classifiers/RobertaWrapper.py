173b9c9a4e640fd64e624f5eb87d2f8d8c113c49
vdberg <vdberg@cl.uni-heidelberg.de>
2020-08-03 16:11:19 +0200

    majority vote cross val

--- lib/classifiers/RobertaWrapper.py
+++ lib/classifiers/RobertaWrapper.py
@@ -149,7 +149,6 @@ class RobertaForSequenceClassification(BertPreTrainedModel):
                 loss = loss_fct(logits.view(-1), labels.view(-1))
             else:
                 loss_fct = CrossEntropyLoss()
-                print(labels)
                 loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
             outputs = (loss,) + outputs
 
