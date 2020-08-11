bc446ff18e571959a9c4348d4926df45efe4dee3
vdberg <vdberg@cl.uni-heidelberg.de>
2020-08-11 16:28:44 +0200

    majority vote cross val

--- lib/handle_data/SplitData.py
+++ lib/handle_data/SplitData.py
@@ -348,18 +348,20 @@ def split_input_for_bert(data_dir, recreate, n_voters):
 
     # write data for each fold with only BERT-relevant columns to all.tsv
     for fold in folds:
+        test_ofp = os.path.join(data_dir, f"{fold['name']}_test.tsv")
+        if not os.path.exists(test_ofp):
+            fold['test'].to_csv(test_ofp, sep='\t', index=False, header=False)
+
         for v in range(n_voters):
             train_ofp = os.path.join(data_dir, f"{fold['name']}_{v}_train.tsv")
             dev_ofp = os.path.join(data_dir, f"{fold['name']}_{v}_dev.tsv")
-            test_ofp = os.path.join(data_dir, f"{fold['name']}_test.tsv")
 
             if not os.path.exists(train_ofp) or recreate:
-                fold['train'][v].to_csv(train_ofp, sep='\t', index=False, header=False)
+                fold['train'][0].to_csv(train_ofp, sep='\t', index=False, header=False)
 
             if not os.path.exists(dev_ofp) or recreate:
-                fold['dev'][v].to_csv(dev_ofp, sep='\t', index=False, header=False)
+                fold['dev'][0].to_csv(dev_ofp, sep='\t', index=False, header=False)
 
-            if not os.path.exists(test_ofp) or recreate:
-                fold['test'].to_csv(test_ofp, sep='\t', index=False, header=False)
+
 
     return folds
