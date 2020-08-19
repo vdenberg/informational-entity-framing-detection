60e3dd44ba3951e5642afb953e4f6dab3f47de75
vdberg <vdberg@cl.uni-heidelberg.de>
2020-08-19 12:39:08 +0200

    majority vote cross val

--- experiments/sent_clf/baselines/roberta_majority_vote.py
+++ experiments/sent_clf/baselines/roberta_majority_vote.py
@@ -176,7 +176,7 @@ if __name__ == '__main__':
                                 logger.info(f"  Details: {best_val_res}")
                                 logger.info(f"  Logging to {LOG_NAME}")
 
-                                FORCE = True
+                                FORCE = False
                                 if not os.path.exists(best_model_loc) or FORCE:
                                     model = RobertaForSequenceClassification.from_pretrained(ROBERTA_MODEL,
                                                                                              cache_dir=CACHE_DIR,
@@ -236,7 +236,7 @@ if __name__ == '__main__':
                                         if dev_mets['f1'] > best_val_res['f1']:
                                             best_val_res.update(dev_mets)
                                             high_score = '(HIGH SCORE)'
-                                            save_model(model, CHECKPOINT_DIR, name)
+                                            # save_model(model, CHECKPOINT_DIR, name)
 
                                         logger.info(f'{epoch_name}: {dev_perf} {high_score}')
 
