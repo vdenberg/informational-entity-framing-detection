7fe76a7d94f86a3fd44fe8af6b7f9b3c4638f6a8
vdberg <vdberg@cl.uni-heidelberg.de>
2020-08-28 14:35:13 +0200

    majority vote cross val

--- experiments/sent_clf/cam/cim_ensemble.py
+++ experiments/sent_clf/cam/cim_ensemble.py
@@ -126,7 +126,7 @@ parser.add_argument('-emb', '--embedding_type', type=str, help='Options: avbert|
 # TRAINING PARAMS
 parser.add_argument('-lex', '--lex', action='store_true', default=False, help='lex')
 parser.add_argument('-context', '--context_type', type=str, help='Options: article|story', default='article')
-parser.add_argument('-cam_type', '--cam_type', type=str, help='Options: cam|cam+|cam++|cam+*|cam+#', default='cam')
+parser.add_argument('-cam_type', '--cam_type', type=str, help='Options: cam|cam+|cam++|cam+*|cam+#', default='cam+')
 parser.add_argument('-base', '--base', type=str, help='Options: base|tapt', default='base')
 
 parser.add_argument('-mode', '--mode', type=str, help='Options: train|eval|debug', default='train')
