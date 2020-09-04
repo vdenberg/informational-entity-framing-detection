af21898bb44291a528137ee74731449d0f717cee
vdberg <vdberg@cl.uni-heidelberg.de>
2020-09-04 13:45:14 +0200

    majority vote cross val

--- experiments/sent_clf/cam/cim_ensemble.py
+++ experiments/sent_clf/cam/cim_ensemble.py
@@ -110,7 +110,7 @@ parser = argparse.ArgumentParser()
 
 # DATA PARAMS
 parser.add_argument('-spl', '--split_type', help='Options: fan|berg|both', type=str, default='berg')
-parser.add_argument('-n_voters', '--n_voters', help='Nr voters when splitting', type=int, default=7)
+parser.add_argument('-n_voters', '--n_voters', help='Nr voters when splitting', type=int, default=1)
 parser.add_argument('-subset', '--subset_of_data', type=float, help='Section of data to experiment on', default=1.0)
 parser.add_argument('-pp', '--preprocess', action='store_true', default=False, help='Whether to proprocess again')
 
