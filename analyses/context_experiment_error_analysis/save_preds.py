1f65d31bf3eff1d5b13da0bef3a3e4a2ad0faab6
vdberg <vdberg@cl.uni-heidelberg.de>
2020-09-18 11:40:14 +0200

    save new cov preds

--- analyses/context_experiment_error_analysis/save_preds.py
+++ analyses/context_experiment_error_analysis/save_preds.py
@@ -294,7 +294,7 @@ data.index = data.sentence_ids.values
 
 spl = Split(data, which=SPLIT_TYPE, subset=SUBSET, recreate=PREPROCESS, n_voters=1)
 folds = spl.apply_split(features=['story', 'source', 'id_num', 'art_context_doc_num', 'cov1_context_doc_num', 'cov2_context_doc_num', 'token_ids', 'token_mask', 'position', 'quartile', 'src_num'])
-
+folds = [folds[0]]
 NR_FOLDS = len(folds)
 
 for fold in folds:
