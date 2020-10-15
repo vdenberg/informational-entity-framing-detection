18a45547bf6130db16c087292ddd21d63e6ea1e8
vdberg <vdberg@cl.uni-heidelberg.de>
2020-10-15 20:57:14 +0200

    save preds

--- lib/classifiers/ContextAwareClassifier.py
+++ lib/classifiers/ContextAwareClassifier.py
@@ -199,6 +199,7 @@ class ContextAwareModel(nn.Module):
             # query = target_sent_reps.unsqueeze(1)
             # proj_key = self.attention.key_layer(sentence_representations) #in tutorial: encoder_hidden
             # mask = (contexts != self.pad_index).unsqueeze(-2) #in tutorial: src
+            print(self.cam_type)
             if self.cam_type == 'cim':
                 context_and_target_rep = torch.cat((target_sent_reps, context_reps), dim=-1)
                 # context_and_target_rep, attn_probs = self.attention(query=target_sent_reps, proj_key=proj_key,
